578054478da065924300dbdc9504ba6ef9a47c57
Pua xian xun <xianxun.22@ichat.sp.edu.sg>
2023-10-24 13:36:13 +0800

    Lab-2

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,5 @@
+import statistics
+
 print("ET0735 (DevOps for AIoT) - Lab2 - Introduction to Python")
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67,32)")
@@ -24,24 +26,15 @@ def sort_temperature():
     print("sort_temperature")
 
 def calc_median_temperature(float_list):
-    sorted_list = sorted(float_list)
+   print("Median =" + str(statistics.median(float_list)))
 
-    if len(sorted_list) % 2 == 0:
-        mid = len(sorted_list) // 2
-        median = (sorted_list[mid - 1] + sorted_list[mid]) / 2
-    else:
-        mid = len(sorted_list) // 2
-        median = sorted_list[mid]
-
-    return median
 
 
 display_main_menu()
 x = get_user_input()
 min , max = find_min_max(x)
 average = calc_average(x)
-median = calc_median_temperature(x)
 print("Min value = " + str(min))
 print("Max value = " + str(max))
 print("Average = " + str(average))
-print("Median = " + str(median))
+calc_median_temperature(x)
